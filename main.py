c4d70bca4ed79926cec6728d853d9f7cafda6fa5
Navneet013 <imnavneet509@gmail.com>
2024-05-21 20:18:19 +0530

    hello

---
+++ main.py
@@ -0,0 +1,6 @@
+print("hello everyone \n kaise ho")
+print(5)
+print(12*3,15-12)
+print("sum is \"add\" ")
+print("nav",6,sep,)
+# this is a comment 
---
+++ test.js
@@ -0,0 +1,20 @@
+console.log("Hello world")
+// we use console.log for print
+alert("this is web")
+
+// we use let for taking two numbers
+let number1 = 10;
+let number2 = 5;
+
+// Perform addition
+let additionResult = number1 + number2;
+console.log("Addition of " + number1 + " and " + number2 + " is: " + additionResult);
+
+// Perform subtraction
+let subtractionResult = number1 - number2;
+console.log("Subtraction of " + number2 + " from " + number1 + " is: " + subtractionResult);
+
+for (let i = 0; i < 5; i++) {
+    console.log('The number is ' + i);
+  }
+  
